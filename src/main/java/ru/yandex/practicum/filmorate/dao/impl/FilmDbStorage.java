3e7f56fb39caddf3ec87b97bad3ef0b8d46eebad
Artem <ustavshikovartem@yandex.ru>
2023-03-07 02:07:09 +0300

    11_ТЗ_Версия_1

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
